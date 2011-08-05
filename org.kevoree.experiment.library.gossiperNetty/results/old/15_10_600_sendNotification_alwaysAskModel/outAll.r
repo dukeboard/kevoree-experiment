da4311d7b37cce4cd88d1c78b5341a1868101f4c
Erwan Daubert <erwan.daubert@gmail.com>
2011-08-05 17:05:40 +0200

    delete all datas

--- org.kevoree.experiment.library.gossiperNetty/results/old/15_10_600_sendNotification_alwaysAskModel/outAll.r
+++
@@ -1,7 +0,0 @@
-propDelais <- c(0)
-netSize <- c()
-
-
-library(Hmisc)
-bpplot(propDelais,main="Downtime propagation delay")
-boxplot(propDelais, propDelais, names = c("a", "b"), horizontal = TRUE, ylab = "factors", main = "title")

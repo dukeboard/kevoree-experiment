bd68e3d6630ebaf30d4de6926d7de86c56b29406
bendavid-olivier <olivier-nathanael.ben_david@inria.fr>
2012-05-23 00:48:24 +0200

    trying to define an exchange model

--- org.kevoree.experiment.p2p/src/main/java/peer/Peer.java
+++ org.kevoree.experiment.p2p/src/main/java/peer/Peer.java
@@ -1,5 +1,6 @@
 package peer;
 
+import org.kevoree.ServicePortType;
 import org.kevoree.annotation.ComponentType;
 import org.kevoree.annotation.Library;
 import org.kevoree.annotation.Port;
@@ -12,15 +13,19 @@ import org.kevoree.annotation.Start;
 import org.kevoree.annotation.Stop;
 import org.kevoree.annotation.Update;
 import org.kevoree.framework.AbstractComponentType;
+import org.kevoree.framework.MessagePort;
 
 
 
 @Library(name = "XACML")
 @ComponentType()
 @Provides({ 
-	@ProvidedPort(name = "download", type = PortType.MESSAGE)
+	@ProvidedPort(name = "uploadRequest", type = PortType.MESSAGE),
+	@ProvidedPort(name = "download", type = PortType.MESSAGE),
+	@ProvidedPort(name = "trigger", type = PortType.MESSAGE)
 })
 @Requires({
+	@RequiredPort(name = "downloadRequest", type = PortType.MESSAGE, optional = true),
 	@RequiredPort(name = "upload", type = PortType.MESSAGE, optional = true)
 })
 
@@ -39,10 +44,28 @@ public class Peer extends AbstractComponentType {
 	public void update() {
 	}
 	
+	@Port(name = "trigger")
+	public void trigger(Object o) {
+		getPortByName("downloadRequest", MessagePort.class).process("test");
+	}
+	
+	
+	@Port(name = "uploadRequest")
+	public void uploadRequest(Object o) {
+		if(o.toString().equals("test")){
+			getPortByName("upload",MessagePort.class).process("send test");
+		}
+	}
 	
 	@Port(name = "download")
 	public void download(Object o) {
-		
+		System.out.println("downloaded : "+o.toString());
+	}
+	
+	
+	public String download(String id){
+		System.out.println(id);
+		return id;
 	}
 	
 }
